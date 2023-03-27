4d9d02174cc9e648674a9ad1678f7167b459c8a1
neronhat <neronhat2907@gmail.com>
2023-03-27 16:43:50 +0700

    v1c

--- New folder/Untitled3.cpp
+++ New folder/Untitled3.cpp
@@ -1,13 +1,25 @@
 #include<iostream>
 using namespace std;
+int a;
+int b;
+void tinh()
+{
+	cout<<a+b;
+}
+
+
+
 
 int main()
 {
+
+	
+	cout<<"a";
+	cin>>a;
+	cout<<"b";
+	cin>>b;
+	tinh();
 	cout<<"hello ban";
 	system("pause");
 	
-<<<<<<< HEAD
 }
-=======
-}
->>>>>>> master
