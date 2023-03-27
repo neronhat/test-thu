a099dbbd232029bd27aba8400da62049ffb39e6f
neronhat <neronhat2907@gmail.com>
2023-03-27 16:14:11 +0700

    V1a

---
+++ New folder/Untitled3 - Copy.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+using namespace std;
+
+
+int a;
+int b;
+void tinh()
+{
+	cout<<a+b;
+}
+
+
+
+
+int main()
+{
+	cout<<"hello ban";
+	
+	cout<<"a";
+	cin>>a;
+	cout<<"b";
+	cin>>b;
+	tinh();
+	
+	system("pause");
+	
+}
